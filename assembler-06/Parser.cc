fec4a4b2aedc86db7ca13d0e6e119db7e89778d0
Dulguun <dulguunb@protonmail.com>
2020-06-02 22:02:29 +0200

    Partially Implemented
    Partially implemented API

--- assembler-06/Parser.cc
+++ assembler-06/Parser.cc
@@ -3,7 +3,78 @@ using namespace std;
 Parser::Parser(string rawProgram):rawProgram(rawProgram){
   currentLetter = rawProgram.begin();
 }
+bool Parser::hasMoreCommands(){
+  return currentLetter != rawProgram.end();
+}
+Command Parser::commandType(){
+    if(*currentLetter == '@'){
+      return Command::A;
+    }
+    auto letter = currentLetter;
+    if(isdigit(*letter) || *letter == '!' || *letter = '-' || isupper(*letter))
+    {
+      return Command::C;
+    }
+    if(*currentLetter == '('){
+      return Command::L;
+    }
+}
+string Parser::symbol(){
+  auto currentCommandType = commandType();
+  if(currentCommandType == Command::A){
+      string result = "";
+      // result+=*currentLetter;
+      advance();
+      while(isdigit(*currentLetter) || *currentLetter != '\n'){ // constant
+        result+=*currentLetter;
+        advance();
+      }
+    return result;
+  }
+  if (currentCommandType == Command::L){
+      string result = "";
+      // result+=*currentLetter;
+      advance();
+      while(*currentLetter != ')'){ // constant
+        result+=*currentLetter;
+        advance();
+      }
+  }
+  return "";
+}
+string Parser::dest(){
+  if (commandType() == Command::C){
+    string result = "";
+    while(*currentLetter != '='){
+      result+=*currentLetter;
+      advance();
+    }
+    advance();
+    return result;
+  }
+  return "";
+}
+string Parser::comp(){
+  if(commandType() == Command::C){
+    string result;
+    while(*currentLetter != ';' || *currentLetter != '\n'){
+      result+=*currentLetter;
+      advance();
+    }
+    advance();
+    return result;
+  }
+  return "";
+}
+string Parser::jump(){
+  if (commandType() == Command::C){
+
+  }
+}
 void Parser::advance(){
+  if (hasMoreCommands()){
+    currentLetter++;
+  }
   // auto letter = rawProgram.begin();
   // bool isEnd = false;
   // while(!isEnd){
@@ -27,14 +98,7 @@ void Parser::advance(){
 
   //     }
   //   }
-  //   if(*letter == '@'){
-  //     string constant = "";
-  //     constant+=*letter;
-  //     letter++;
-  //     while(isdigit(*letter)){ // constant
-  //       constant+=*letter;
-  //       letter++;
-  //     }
+  
   //     while(isalnum(*letter)){ //variable?
 
   //     }
