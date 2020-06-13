1c3aa2d9847e2355bbb860e64c11609bec1ae79d
Dulguun <dulguunb@protonmail.com>
2020-06-13 20:48:33 +0200

    Seperated CommandType into a new file

---
+++ vmTranslator-7/CommandType.h
@@ -0,0 +1,14 @@
+#ifndef COMMANDTYPE_H
+#define COMMANDTYPE_H
+enum CommandType{
+  C_ARITHMETIC,
+  C_PUSH,
+  C_POP,
+  C_LABEL,
+  C_GOTO,
+  C_IF,
+  C_FUNCTION,
+  C_RETURN,
+  C_CALL
+};
+#endif
